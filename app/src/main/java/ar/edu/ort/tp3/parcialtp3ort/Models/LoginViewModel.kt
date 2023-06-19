75c90394cd55c4a3a530f28fb0ad24c3f73643d2
Alejo Conde <alejo.conde.97@gmail.com>
2023-06-19 17:58:02 -0300

    feat: initial implementation

--- app/src/main/java/ar/edu/ort/tp3/parcialtp3ort/Models/LoginViewModel.kt
+++ app/src/main/java/ar/edu/ort/tp3/parcialtp3ort/Models/LoginViewModel.kt
@@ -2,6 +2,7 @@ package ar.edu.ort.tp3.parcialtp3ort.Models
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseUser
 
 class LoginViewModel:ViewModel() {
 
@@ -9,8 +10,22 @@ class LoginViewModel:ViewModel() {
     val clave = MutableLiveData<String>()
 
 
-    fun guardarCredenciales(nombre: String, password: String) {
+    /*fun guardarCredenciales(nombre: String, password: String) {
         usuario.value = nombre
         clave.value = password
+    }*/
+    fun guardarCredenciales(user: FirebaseUser?) {
+        user?.let {
+            // Name, email address, and profile photo Url
+            usuario.value = it.displayName
+
+            // Check if user's email is verified
+            //val emailVerified = it.isEmailVerified
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getIdToken() instead.
+            //val uid = it.uid
+        }
     }
 }
--- app/src/main/java/ar/edu/ort/tp3/parcialtp3ort/fragments/LoginFragment.kt
+++ app/src/main/java/ar/edu/ort/tp3/parcialtp3ort/fragments/LoginFragment.kt
@@ -1,35 +1,43 @@
 package ar.edu.ort.tp3.parcialtp3ort.fragments
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
-import androidx.lifecycle.Observer
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import ar.edu.ort.tp3.parcialtp3ort.Models.LoginViewModel
 import ar.edu.ort.tp3.parcialtp3ort.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class LoginFragment : Fragment() {
+    private lateinit var auth: FirebaseAuth
     private lateinit var viewModel: LoginViewModel
-    lateinit var viewLogin:View;
+    lateinit var viewLogin:View
     lateinit var btnLogin: Button
     lateinit var user:EditText
     lateinit var pass:EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth = Firebase.auth
 
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         viewLogin = inflater.inflate(R.layout.fragment_login, container, false)
         btnLogin = viewLogin.findViewById(R.id.button_login)
@@ -47,23 +55,52 @@ class LoginFragment : Fragment() {
 
         viewModel = ViewModelProvider(requireActivity()).get(LoginViewModel::class.java)
 
-        viewModel.usuario.observe(viewLifecycleOwner, Observer { result ->
-            user.setText(result.toString())
-        })
-
-        viewModel.usuario.observe(viewLifecycleOwner, Observer { result ->
-            user.setText(result.toString())
-        })
-
         btnLogin.setOnClickListener{
-            viewModel.guardarCredenciales(user.text.toString(), pass.text.toString())
-           val action =  LoginFragmentDirections.actionLoginFragmentToMainFragment()
-            viewLogin.findNavController().navigate(action)
+            signIn(user.text.toString(), pass.text.toString())
         }
 
 
 
     }
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            reload()
+        }
+    }
 
+
+    private fun updateUI(user: FirebaseUser) {
+        viewModel.guardarCredenciales(user)
+        val action =  LoginFragmentDirections.actionLoginFragmentToMainFragment()
+        viewLogin.findNavController().navigate(action)
+    }
+
+    private fun reload() {
+
+    }
+
+    private fun signIn(email: String, password: String) {
+        Log.d("The email", email)
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this.requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user!!)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        this.context,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+    }
 
 }

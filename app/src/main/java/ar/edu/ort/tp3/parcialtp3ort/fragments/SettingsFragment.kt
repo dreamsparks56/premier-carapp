d6208c82353054b014e2dc44f6036b8d0f72b7c5
Alejo Conde <alejo.conde.97@gmail.com>
2023-07-17 01:19:55 -0300

    feat: initial localization implementation

--- app/src/main/java/ar/edu/ort/tp3/parcialtp3ort/fragments/SettingsFragment.kt
+++ app/src/main/java/ar/edu/ort/tp3/parcialtp3ort/fragments/SettingsFragment.kt
@@ -5,11 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.RadioGroup
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.LocaleListCompat
 import ar.edu.ort.tp3.parcialtp3ort.R
 
 
 
 class SettingsFragment : Fragment() {
+    lateinit var v: View
+    lateinit var languageEn: Button
+    lateinit var languageEs: Button
+    lateinit var languageGroup: RadioGroup
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,9 +28,43 @@ class SettingsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_configuracion, container, false)
+        v = inflater.inflate(R.layout.fragment_configuracion, container, false)
+        languageEn = v.findViewById(R.id.languageEnglish)
+        languageEs = v.findViewById(R.id.languageSpanish)
+        languageGroup = v.findViewById(R.id.languageGroup)
+        getLanguage()
+
+        languageEn.setOnClickListener {
+            setLanguage("en")
+        }
+        languageEs.setOnClickListener {
+            setLanguage("es")
+        }
+        return v
+    }
+
+
+
+    private fun setLanguage(language: String) {
+        val appLocale: LocaleListCompat = LocaleListCompat.forLanguageTags(language)
+
+        // Call this on the main thread as it may require Activity.restart()
+        AppCompatDelegate.setApplicationLocales(appLocale)
+    }
+
+    private fun getLanguage() {
+        // Call this to get the selected locale and display it in your App
+        val selectedLocale = AppCompatDelegate.getApplicationLocales()[0]
+        when (selectedLocale?.language) {
+            "en" -> languageGroup.check(languageEn.id)
+            "es" -> languageGroup.check(languageEs.id)
+        }
+
+
+
+
     }
 
 

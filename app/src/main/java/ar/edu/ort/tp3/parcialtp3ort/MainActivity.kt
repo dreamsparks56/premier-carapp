afb84a6bea17a5aa33dccbace1b4c6c1a12a1f32
Alejo Conde <alejo.conde.97@gmail.com>
2023-07-24 00:38:47 -0300

    feature: migrate to preference screen

--- app/src/main/java/ar/edu/ort/tp3/parcialtp3ort/MainActivity.kt
+++ app/src/main/java/ar/edu/ort/tp3/parcialtp3ort/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
         //val db : DBHelper = DBHelper(getApplicationContext(),null)
 
+
     }
 
 
--- app/src/main/java/ar/edu/ort/tp3/parcialtp3ort/fragments/SettingsFragment.kt
+++ app/src/main/java/ar/edu/ort/tp3/parcialtp3ort/fragments/SettingsFragment.kt
@@ -1,55 +1,40 @@
 package ar.edu.ort.tp3.parcialtp3ort.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+import android.util.Log
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.RadioGroup
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.cardview.widget.CardView
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 import androidx.core.os.LocaleListCompat
-import androidx.lifecycle.ViewModelProvider
-import ar.edu.ort.tp3.parcialtp3ort.Models.LoginViewModel
+import androidx.preference.ListPreference
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
 import ar.edu.ort.tp3.parcialtp3ort.R
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
     lateinit var v: View
-    lateinit var languageEn: Button
-    lateinit var languageEs: Button
-    lateinit var languageGroup: RadioGroup
-    lateinit var emailButton: CardView
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        // Inflate the layout for this fragment
-        v = inflater.inflate(R.layout.fragment_configuracion, container, false)
-        languageEn = v.findViewById(R.id.languageEnglish)
-        languageEs = v.findViewById(R.id.languageSpanish)
-        languageGroup = v.findViewById(R.id.languageGroup)
-        emailButton = v.findViewById(R.id.email_button)
-        getLanguage()
-
-        languageEn.setOnClickListener {
-            setLanguage("en")
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
+        val localePreference: ListPreference?  = findPreference("language")
+        val themePreference: ListPreference? = findPreference("dark_theme")
+        getLanguage(localePreference!!)
+        localePreference.setOnPreferenceChangeListener { _, newValue ->
+            setLanguage(newValue.toString())
+            true
         }
-        languageEs.setOnClickListener {
-            setLanguage("es")
+        themePreference?.setOnPreferenceChangeListener { _, newValue ->
+            Log.d("new value", newValue.toString())
+            when(newValue.toString()) {
+                "l" -> AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+                "d" -> AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+                "sd" -> AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_FOLLOW_SYSTEM)
+            }
+            true
         }
-        setupChangeEmail()
-        return v
     }
 
 
@@ -61,20 +46,24 @@ class SettingsFragment : Fragment() {
         AppCompatDelegate.setApplicationLocales(appLocale)
     }
 
-    private fun getLanguage() {
-        // Call this to get the selected locale and display it in your App
-        val selectedLocale = AppCompatDelegate.getApplicationLocales()[0]
-        when (selectedLocale?.language) {
-            "en" -> languageGroup.check(languageEn.id)
-            "es" -> languageGroup.check(languageEs.id)
+    private fun getLanguage(preference: ListPreference) {
+        val locale = AppCompatDelegate.getApplicationLocales()[0]
+        preference.summaryProvider =
+            Preference.SummaryProvider<ListPreference> { _ ->
+                when (locale?.language) {
+                    preference.entryValues[0] -> preference.entries[0]
+                    preference.entries[1] -> preference.entries[1]
+                    else -> {preference.entries[0]}
+                }
+            }
         }
-    }
 
-    private fun setupChangeEmail() {
+
+    /*private fun setupChangeEmail() {
         val userViewModel = ViewModelProvider(requireActivity())[LoginViewModel::class.java]
         val emailText: TextView = emailButton.findViewById(R.id.email_button_placeholder)
         setupEmailText(userViewModel.email.value!!, emailText)
-        val newEmail = userViewModel.email.value!!.replaceFirstChar { 'a' }
+        val newEmail = userViewModel.email.value!!.replaceFirstChar { 'p' }
         emailButton.setOnClickListener {
             val emailDialog = MaterialAlertDialogBuilder(requireContext())
             emailDialog.setTitle("Does nothing")
@@ -89,14 +78,14 @@ class SettingsFragment : Fragment() {
             emailDialog.show()
         }
 
-    }
+    }*/
 
-    private fun setupEmailText(email: String, textView: TextView) {
+    /*private fun setupEmailText(email: String, textView: TextView) {
         var textBefore: String = email.substringBefore('@')
             textBefore = textBefore.replaceRange(0..textBefore.length/2, "********")
         val textAfter: String = textBefore + "@" +  email.substringAfter('@')
         textView.text = textAfter
-    }
+    }*/
 
 
 }
